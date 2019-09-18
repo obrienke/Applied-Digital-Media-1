35d823d676fd098910784c31af1a51044e748763
obrienke <kevin.obrien@gmit.ie>
2019-09-18 12:54:03 +0100

    Add files via upload

---
+++ week3/lecture.js
@@ -0,0 +1,19 @@
+let number1 = Number(prompt("Enter first number"));
+let number2 = Number(prompt("Enter second number"));
+document.write("<p>"+number1+" + "+number2+" = "+(number1 + number2)+"</p>");
+
+
+let num1 = prompt("Enter first number");
+let num2 = prompt("Enter second number");
+document.write("<p>"+num1+" * "+num2+" = "+(num1 * num2)+"</p>");
+
+alert(number1+" + "+number2+" = "+(number1 + number2)+"\n"+num1+"* "+num2+" = "+(num1 * num2));
+
+var result = 5 + (2 * 4) - (9 / 3);
+
+var date = new Date();
+
+document.write("<p>"+date.getDay()+"</p>");
+document.write("<p>"+date.getFullYear()+"</p>");
+document.write("<p>"+date+"</p>");
+
