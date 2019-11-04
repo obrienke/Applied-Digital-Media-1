afc07ae4ea42cc932229d9e3ca3b1be65a091b04
obrienke <kevin.obrien@gmit.ie>
2019-11-04 09:53:29 +0000

    week 9

---
+++ Decision.java
@@ -0,0 +1,10 @@
+
+public class Decision{
+        public static void main(String[] args){
+            int x = 1;
+
+            if(x <= 1){
+                System.out.println("Value assigned to x is 1");
+            }
+        }
+}
