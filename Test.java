4ea494ececbf2c6a304b9739e9fe46eb75e5d098
obrienke <kevin.obrien@gmit.ie>
2020-02-19 18:45:44 +0000

    nested loop example

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test{
+    public static void main(String[] args){
+        int i = 1;
+        int j = 1;
+        while(i <= 5){
+            while(j <= 5){
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            j = 1;
+            i++;
+        }
+    }
+}
