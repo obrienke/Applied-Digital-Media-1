c927bb7e955d3e2058c5e0a2db7cb2ba3799bba9
obrienke <kevin.obrien@gmit.ie>
2020-09-02 14:21:37 +0000

    Clearing repository for start of academic year

--- week9/ex4.js
+++
@@ -1,7 +0,0 @@
-var numbers = [ 10, 20, 30, 40, 50 ];
-
-document.write("Contents of numbers array:<br><br>");
-
-for(var i = 0; i < numbers.length; i++){
-	document.write("element "+(i+1)+": "+numbers[i]+"<br>");
-}
