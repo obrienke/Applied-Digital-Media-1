f9519cac3e641a243a06ec11680a7f64ec9e58ae
obrienke <kevin.obrien@gmit.ie>
2020-10-05 20:43:18 +0100

    week 2 lab assister
    Contains javascript which students can use for the week 2 exercises

---
+++ week2/week2Lab.js
@@ -0,0 +1,26 @@
+alert("Hello JavaScript");
+document.write("Hello JavaScript");
+
+document.write("<h1>Hello JavaScript</h1>");
+document.write("<h2>Hello JavaScript</h2>");
+
+let orderedList = "<ol>";
+orderedList += "<li>";
+orderedList += "first";
+orderedList += "</li>";
+orderedList += "<li>";
+orderedList += "second";
+orderedList += "</li>";
+orderedList += "</ol>";
+
+document.getElementById("list").innerHTML = orderedList;
+
+let num1 = prompt("Enter first number");
+let num2 = prompt("Enter second number");
+let result = Number(num1) + Number(num2);
+
+document.write("<h3>");
+document.write(num1+" + "+num2+" = "+result);
+document.write("</h3>");
+
+alert("Line 1 \n Line 2");
