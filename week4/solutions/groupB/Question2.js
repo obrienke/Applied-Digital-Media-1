0be50eeeab407d238e3c8b923c22bafbe9f7bb00
obrienke <kevin.obrien@gmit.ie>
2022-11-23 11:19:44 +0000

    Assessment 1 solutions

---
+++ week4/solutions/groupB/Question2.js
@@ -0,0 +1,7 @@
+// Question 2
+let x = 11;
+let y = 100;
+let z = 1000;
+let result = x + y + z;
+
+alert(x + " + " + y + " + " + z + " = " + result);
---
+++ week4/solutions/groupB/Question4.js
@@ -0,0 +1,6 @@
+// Question 4
+let pounds = prompt("Enter number of pounds sterling you want to convert to euro");
+let conversion = 1.14;
+let euro = pounds * conversion;
+
+alert(pounds + " pounds sterling is equivalent to:\n"+euro+" euro\n(" + pounds + " * " + conversion + ") = " + euro);
